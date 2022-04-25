ef1aa30b012c1df8c0c979e2d5be76c61fe9ab74
Joel-Konverge <joel.noronha@konverge.ai>
2022-04-26 03:29:18 +0530

    error fixed

--- practise/quicksort.py
+++ practise/quicksort.py
@@ -24,32 +24,7 @@ def quicksort(array, st, en):
     quicksort(array,par+1,en)
     return array
 
-arr=[
-def partition(arr,st,j):
-    p=st
-    i=st+1
-    while j>i:
-        while i<j and arr[i]<arr[p]:
-            i+=1
-        while j>0 and arr[j]>arr[p]:
-            j-=1
-        if j<i:
-            break
-        arr[i],arr[j]=arr[j],arr[i]
-    if arr[p]>arr[j]:
-        arr[p],arr[j]=arr[j],arr[p]
-    return j
-
-
-
-def quicksort(array, start, end):
-    if start >= end:
-        return
-    p = partition(array, start, end)
-    quicksort(array, start, p-1)
-    quicksort(array, p+1, end)
-
 
 arr=[9, -3, 5, 2, 6, 8, -6, 1, 3]
-quicksort(arr,0,len(arr)-1)]
+quicksort(arr,0,len(arr)-1)
 print(quicksort(arr,0,len(arr)-1))
